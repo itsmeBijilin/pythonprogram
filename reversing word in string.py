1616fd874276d9d750af5381d1a34b76a14445cb
itsmeBijilin <46573772+itsmeBijilin@users.noreply.github.com>
2019-02-21 22:07:19 +0530

    Update reversing word in string.py

--- reversing word in string.py
+++ reversing word in string.py
@@ -1,9 +1,9 @@
-a=input().split()
-b=[]
-for x in a:
-    b.append(x[::-1])
-for i in range(0,len(b)):
-    if(i==len(b)-1):
-        print(b[i],end=(""))
+x=input().split()
+y=[]
+for z in x:
+    y.append(z[::-1])
+for i in range(0,len(y)):
+    if(i==len(y)-1):
+        print(y[i],end=(""))
     else:
-        print(b[i],end=(" "))
+        print(y[i],end=(" "))
